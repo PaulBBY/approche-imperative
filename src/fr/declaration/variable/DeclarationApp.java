03357cb3583467ae1a7732fce2ca29c2c87fc793
PaulBBY <paul.baby1995@gmail.com>
2023-10-04 14:54:22 +0200

    v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte monByte = 1;
+		short monShort = 2; 
+		int monInt = 3;
+		long monLong = 4l;
+		float monFloat = 0.5f;
+		double monDouble = 0.6;
+		char monChar = '7';
+		boolean monBoolean = true;
+		String monString = "Huit";
+		
+		System.out.println("Soixante-dix-"+monString);
+		
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		System.out.println(randomString);
+		
+		randomString = "Voici le résultat d'un calcul :\n1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
